27af6eb3cff308514bef4fdaee29ed673a5367c2
rohitdeepu17 <rohitdeepu17@gmail.com>
2015-11-14 21:38:18 +0530

    Change of Buttons on Home Page and Added Contact Us and TnCs Activity
    Modified:-
    HomePage : Buttons have been arranged in pairs, with images over them.
    Icon of the application changed
    
    Newly added:-
    TermsAndConditions activity
    ContactUs activity : links to phone number and email working fine on
    this page

--- Android/ScrapMan/app/src/main/java/com/example/rohitsingla/scrapman/HomePage.java
+++ Android/ScrapMan/app/src/main/java/com/example/rohitsingla/scrapman/HomePage.java
@@ -2,6 +2,7 @@ package com.example.rohitsingla.scrapman;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -10,18 +11,44 @@ import android.widget.Button;
 
 
 public class HomePage extends Activity {
-
-    Button buttonPriceList, buttonRequestPickup, buttonAccountSettings, buttonCheckPastRequests;
+    Button buttonPriceList, buttonRequestPickup, buttonAccountSettings, buttonCheckPastRequests, buttonTnCs, buttonContactUs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
 
+        Drawable img;
+
         buttonPriceList = (Button)findViewById(R.id.button_price_list);
-        buttonRequestPickup = (Button)findViewById(R.id.button_request_pickup);
-        buttonAccountSettings = (Button)findViewById(R.id.button_account_settings);
-        buttonCheckPastRequests = (Button)findViewById(R.id.button_check_past_requests);
+        img = getApplicationContext().getResources().getDrawable( R.drawable.price_list );
+        img.setBounds( 0, 30, 230, 230 );
+        buttonPriceList.setCompoundDrawables(null, img, null, null);
+
+        buttonRequestPickup = (Button) findViewById(R.id.button_request_pickup);
+        img = getApplicationContext().getResources().getDrawable( R.drawable.request_pickup );
+        img.setBounds( 0, 30, 230, 230 );
+        buttonRequestPickup.setCompoundDrawables(null, img, null, null);
+
+        buttonAccountSettings = (Button) findViewById(R.id.button_account_settings);
+        img = getApplicationContext().getResources().getDrawable( R.drawable.account_settings );
+        img.setBounds( 0, 30, 230, 230 );
+        buttonAccountSettings.setCompoundDrawables(null, img, null, null);
+
+        buttonCheckPastRequests = (Button) findViewById(R.id.button_check_past_requests);
+        img = getApplicationContext().getResources().getDrawable( R.drawable.past_requests );
+        img.setBounds( 0, 30, 230, 230 );
+        buttonCheckPastRequests.setCompoundDrawables(null, img, null, null);
+
+        buttonTnCs = (Button) findViewById(R.id.button_terms_and_conditions);
+        img = getApplicationContext().getResources().getDrawable( R.drawable.terms_and_conditions );
+        img.setBounds( 0, 30, 230, 230 );
+        buttonTnCs.setCompoundDrawables(null, img, null, null);
+
+        buttonContactUs = (Button) findViewById(R.id.button_contact_us);
+        img = getApplicationContext().getResources().getDrawable( R.drawable.contact_us );
+        img.setBounds( 0, 30, 230, 230 );
+        buttonContactUs.setCompoundDrawables(null, img, null, null);
 
         buttonPriceList.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +78,22 @@ public class HomePage extends Activity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(HomePage.this, CheckPastRequests.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonTnCs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, TermsAndConditions.class);
+                startActivity(intent);
+            }
+        });
+
+        buttonContactUs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomePage.this, ContactUs.class);
                 startActivity(intent);
             }
         });
